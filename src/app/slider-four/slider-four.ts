2af339e587ace2cca2b7840f7d962679c8248aa5
mhumayon054 <mhumayon054@gmail.com>
2025-10-16 18:52:58 +0500

    slider images

--- src/app/slider-four/slider-four.ts
+++ src/app/slider-four/slider-four.ts
@@ -20,8 +20,8 @@ export class SliderFour implements OnInit, OnDestroy {
 
   images = [
     {
-      defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758735067/fallwinter5_rmesxj.webp",
-      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
+      defaultUrl: "../../assets/looks/Look-9.1-768x1152.jpeg.webp",
+      hoverUrl: "../../assets/looks/Look-9.2-600x900.jpeg",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733782/fallwinter3_kfb0ee.webp",
@@ -33,19 +33,19 @@ export class SliderFour implements OnInit, OnDestroy {
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733798/Look_2.1_lbvjge.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733800/Look_2.2_wwyqzq.jpg",
+      hoverUrl: "../../assets/looks/Look-2.2-600x900.jpeg.webp",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733809/Look_3.1_f6usaw.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733811/Look_3.2_wwyqzq.jpg",
+      hoverUrl: "../../assets/looks/Look-3.2-600x900.jpeg.webp",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733800/Look_4.1_cyclai.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733802/Look_4.2_yz9nyi.jpg",
+      hoverUrl: "../../assets/looks/look-4.2-600x900.jpeg.webp",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733818/Look_6.1_trwggm.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733820/Look_6.2_zzm4kq.jpg",
+      hoverUrl: "../../assets/looks/Look-10.1-scaled.jpeg",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733814/Look_7.1_gijgos.jpg",
@@ -53,71 +53,71 @@ export class SliderFour implements OnInit, OnDestroy {
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733807/Look_10.2_kvqz2c.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733805/Look_10.1_q0cxqa.jpg",
+      hoverUrl: "../../assets/looks/Look-10.1-scaled.jpeg",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733826/Look_12.2_kcmq9p.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733824/Look_12.1_oaofal.jpg",
+      hoverUrl: "../../assets/looks/Look-12.1-scaled.jpeg.webp",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733972/Look_13_o93hcp.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733974/Look_13.2_qbwxgq.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733788/Look_1.2_zhzwe1.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733778/fallwinter4_fsdty9.webp",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733824/Look_15_mdndyz.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733826/Look_15.2_hbjzif.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733836/Look_16_yz9nyi.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733838/Look_16_t6uu6d.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733838/Look_18_mtnjli.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733840/Look_18_wko8eq.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733776/Look_19_bagho6.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733774/Look_19_bej2l2.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733762/Look_20_mnriki.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733760/Look_20_ci0gol.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733778/Look_21_axtlfh.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733775/Look_21_bmnmv7.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733805/Look_22_izqgno.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733803/Look_22_ls5djy.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733860/Look_11.2_komkyy.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733858/Look_11.1_gnm7pi.jpg",
+      hoverUrl: "../../assets/Look-11.1-768x1152.jpeg.webp",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733814/Look_23_fxawbc.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733812/Look_23_au9fyn.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733775/Look_24_u3iecq.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733773/Look_24_agh3pi.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733778/Look_25_ypnyy6.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733776/Look_25_oqkp9s.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733799/Look_26_s20b8i.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733797/Look_26_ls5djy.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
     {
       defaultUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733788/Look_27_exa9vm.jpg",
-      hoverUrl: "https://res.cloudinary.com/dzit141xn/image/upload/v1758733786/Look_27_kmfmsv.jpg",
+      hoverUrl: "https://placehold.co/800x1200?text=Hover+Preview",
     },
   ]
 
